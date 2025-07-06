51100a907839072de572b565b2c021186aec0a27
Zuhaib Hanfi <hanfizuhaib@gamil.com>
2025-07-06 16:32:05 +0530

    added seach option

--- components/body.jsx
+++ components/body.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react"
 
 
-function Body (){
+function Body() {
     const [Profile, setProfile] = useState([]);
+    const [numberOfProfiles, updateNumberOfProfiles] = useState("");
 
     async function generateProfile() {
-        const response = await fetch("https://api.github.com/users?per_page=10");
+        const rand = Math.floor(Math.random() * 10000);
+        const response = await fetch(`https://api.github.com/users?since=${rand}&per_page=${numberOfProfiles}`);
         const data = await response.json();
 
         setProfile(data);
@@ -15,21 +17,34 @@ function Body (){
         generateProfile();
     }, []);
 
+
     return (
         <div id="profile">
-            {
-                Profile.map((value) => {
-                    return (
-                        <div key={value.id} className="cards">
-                            <img src={value.avatar_url}></img>
-                            <h2>{value.login}</h2>
-                            <a href={value.html_url} target="_blank">GitHub Profile</a>
-                        </div>
-                    )
-                })
-            }
+            <div id="inputContainer">
+                <input type="text" placeholder="Enter No. of profiles" value={numberOfProfiles} onChange={(e) => updateNumberOfProfiles(e.target.value)} />
+                <button onClick={generateProfile}>Search</button>
+            </div>
+            <div id="cardsContainer" >
+                {
+                    Profile.map((value) => {
+                        return (
+
+                            <div key={value.id} className="cards">
+                                <img src={value.avatar_url}></img>
+                                <h2>{value.login}</h2>
+                                <a href={value.html_url} target="_blank">GitHub Profile</a>
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
 
 export default Body;
+
+
+//UseCallBack & Try catch
+// add one more seach by user name button
