b3bca510bc7dbc0fe9f990570c4b1d960d92e55a
Zuhaib Hanfi <hanfizuhaib@gamil.com>
2025-07-09 14:24:31 +0530

    added finalized app.jsx

--- app.jsx
+++ app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/header";
 import Body from "./components/body";
+import Body2 from "./components/body2";
 import Footer from "./components/footer";
 
 function App() {
